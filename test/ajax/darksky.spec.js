c0fd772246c754841cc71c48f5cc208e08b7240c
Michael van Genderen <mikevg466@gmail.com>
2017-07-26 17:05:13 -0400

    test: first pass at tests

---
+++ test/ajax/darksky.spec.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+
+describe('DarkSky AJAX requests', () => {
+  describe('Current Weather', () => {
+    it('makes calls to the darksky server', () => {});
+    it('uses the secret api key in the call', () => {});
+    it('pulls current weather for a given area', () => {});
+  }); // end describe('Current Weather')
+  // https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+  describe('Forecasted Weather', () => {
+    it('makes calls to the darksky server', () => {});
+    it('uses the secret api key in the call', () => {});
+    it('pulls forecasted weather for a given area', () => {});
+  }); // end describe('Forecasted Weather')
+  // https://api.darksky.net/forecast/[key]/[latitude],[longitude],[time]
+  describe('Historic Weather', () => {
+    it('makes calls to the darksky server', () => {});
+    it('uses the secret api key in the call', () => {});
+    it('pulls historic weather for a given area', () => {});
+  }); // end describe('Historic Weather')
+}); // end describe('DarkSky AJAX requests')
